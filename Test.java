ad024c52f307dfc000e7db1276d77a991d812241
your_username <your_email_address@example.com>
2023-06-30 10:56:02 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,26 @@
+package com.imageinfo.eclear.configuration;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		String pipeSeparatedList="CLG|FINONEPDC|FINACLEPDC|CORPPDC";
+		String regex4 = "(\\b"+(pipeSeparatedList.replace("|", "\\b|\\b"))+"\\b)";
+		
+		System.out.println("regex4 : "+regex4);
+		
+		Pattern p = Pattern.compile(regex4);
+		
+		Matcher m = p.matcher("CLG");
+		System.out.println(m.find());
+		
+		boolean a = Pattern.matches(regex4, "CLG");
+		
+		System.out.println("a : "+a);
+
+	}
+
+}
